e556abb80aa73e164dda5b8e92f3e1a22034217f
Nek1207 <Nek1207@yandex.ru>
2026-04-11 10:49:40 +0500

    v0.3.2 (build202604112)

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -19,6 +19,7 @@ import branch2 from "./../../assets/content/information_branch2.svg"
 import branches from "./../../assets/content/cont_branches.svg"
 import waves from "./../../assets/content/form_waves.svg"
 
+import leafs from "./../../assets/content/cont_leafs.svg"
 import leaf1 from "./../../assets/content/cont_leaf.svg"
 import leaf2 from "./../../assets/content/cont_leaf2.svg"
 
@@ -29,22 +30,23 @@ const Signin = () => {
 
   return (
     <>
-     <section className="signin">
-        <div className="signin__logo">
-            <img className="signin__logo-background" src={waves} alt="background" />
+        <section className="signin">
+          <div className="signin__logo">
+            <img className="signin-background" src={waves} alt="background" />
             <NavLink to="/"><img className="signin__logo-image" src={logo} alt="logo" /></NavLink>
             <h3 className="signin__logo-title">мой рацион</h3>
             <p className="signin__logo-subtitle">подсчёт калорийности</p>
-        </div>
-      <div className="branch branch__signin smoothscrolling-r hidden" ref={el => branchesRefs.current[0] = el}><img className="branch__image" src={branch2} alt="branch2"/></div>
-        <div className="signin__form">
-            <h1 className="heading2 c-white t-center">авторизация</h1>
-            {/* <Input type="password" id="password" name="password" placeholder="укажите здесь Ваш пароль..." value="" styles="password" status="password" isRequired="true" isDisabled="false" /> */}
-            <img className="signin__form__image" src={leaf1} alt="leaf1" />
-            <img className="signin__form__image2" src={leaf2} alt="leaf2" />
-
-        </div>
-     </section>
+          </div>
+          <div className="branch branch__signin smoothscrolling-r hidden" ref={el => branchesRefs.current[0] = el}><img className="branch__image" src={branch2} alt="branch2"/></div>
+          <div className="signin__texts">
+            <div className="signin__texts-inner">
+              <img className="signin__texts__image" src={leaf1} alt="leaf1" />
+              <h1 className="heading2 c-white t-center">авторизация</h1>
+              <img className="signin__texts__image2" src={leaf2} alt="leaf2" />
+            </div>
+          </div>
+      </section>
+      <img className="leafs" src={leafs} alt="leafs" />
     </>
   )
 }
